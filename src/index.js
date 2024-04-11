726531c6ccccdc4082880e90281a194119d7d1b5
guguqueen <guguq59@gmail.com>
2024-04-11 11:05:32 +0200

    added the search funationality using the api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    
+}
+
+
+function searchCity (city){
+    // make the api call and update interface
+let apiKEY = "e2ctfac817c6d3a98601of8420b16363";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+axios.get(apiURL).then(refreshWeather);
+
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+   
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
